d579e7786f3a66bd6350bde8cc07f2339c7c85af
name <fedorovn19@gmail.com>
2024-10-11 19:22:15 +0300

    Prepare for initial release

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export type { UniswapTokenProps } from './types';
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface UniswapTokenProps {
+  symbol: string;
+  price: number;
+  fee: string;
+  networkNameUniswap: string;
+  address: string;
+}
